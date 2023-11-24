7e26bacea2cdeb2f2e6a7dfa65d4ec5dbba3e16d
naochavez02 <109756659+naochavez02@users.noreply.github.com>
2023-11-24 09:46:10 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-//service worker
+//Service worker
 
-if('serviceWorker' in Navigator){
-    console.log('puedes usar los serviceWorker del navegador');
+if('serviceWorker' in navigator){
+    console.log('Puedes usar los serviceWorker del navegador');
 
     navigator.serviceWorker.register('./sw.js')
-                            .then(res=>console.log('serviceWorker cargado correctamente',res))
-                            .then(err=>console.log('ServiceWorker no se ha podido registrar', err))
+                           .then(res => console.log('serviceWorker cargado correctamente', res))
+                           .catch(err => close.log('serviceWorker no se ha podido registrar', err))
 }else{
-    console.log('No puedes usar los serviceWorker del navegador');
+    console.log('NO Puedes usar los ServiceWorker del navegador');
 }
+
 
 //scroll suavizado
 $(document).ready(function(){
+    
     $("#menu a").click(function(e){
         e.preventDefault();
-
-        $("html,body").animate({
-            scrollTop:$($(this).attr('href')).offset().top
+        
+        $("html, body").animate({
+            scrollTop: $($(this).attr('href')).offset().top
         });
         return false;
     });
